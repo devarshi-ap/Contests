28072e56042a27a45fe848c50117107f6e20ad1a
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-09-21 15:30:34 -0400

    Create hailstone_numbers.java

---
+++ Uncategorized DMOJ/Java/hailstone_numbers/hailstone_numbers.java
@@ -0,0 +1,33 @@
+//Basically Collatz Sequence
+
+import java.util.Scanner;
+
+
+
+public class hailstone_numbers
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+
+        int num = sc.nextInt();
+        int count = 0;
+        
+            
+        while(num > 1)//if number isn't already 1...
+        {
+            if(num % 2 == 0)//checks if number is positive
+            {
+                num = num/2;//number is divided by 2
+                count++;
+            }
+            else{//if number isn't even; is odd,
+                num = 3*num + 1;//number is multiplied by 3 and added 1 to.
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        sc.close();
+	}
+}

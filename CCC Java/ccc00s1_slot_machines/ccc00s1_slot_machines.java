3ca3bb08ff46777982ae84c1a6046c77ebfeda31
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-09-21 17:05:07 -0400

    Create ccc00s1_slot_machines.java

---
+++ CCC Java/ccc00s1_slot_machines/ccc00s1_slot_machines.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class ccc00s1_slot_machines {
+	public static void main(String[] args) {
+	    
+		Scanner sc = new Scanner(System.in);
+		
+		int jar = sc.nextInt();
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int z = sc.nextInt();
+		int val = 0;
+
+		while(jar > 0) {
+			if(jar == 0) {
+				break;
+			}
+			
+			x++;
+			val++;
+			jar--;
+			
+			if(x == 35) {
+				jar = jar + 30;
+				x = 0;
+			}
+			if(jar == 0) {
+				break;
+			}
+			
+			y++;
+			val++;
+			jar--;
+			
+			if(y == 100) {
+				jar = jar + 60;
+				y = 0;
+			}
+			
+			if(jar == 0) {
+				break;
+			}
+			
+			z++;
+			val++;
+			jar--;
+			
+			if(z == 10) {
+				jar = jar + 9;
+				z = 0;
+			}	
+		}
+		
+		System.out.printf("Martha plays %d times before going broke.", val);
+        sc.close();
+    }
+}

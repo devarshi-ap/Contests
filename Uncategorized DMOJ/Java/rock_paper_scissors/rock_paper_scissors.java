769c5565a69cdbabd634bb6cd53a314d44350a87
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-09-22 13:57:52 -0400

    Update rock_paper_scissors.java

--- Uncategorized DMOJ/Java/rock_paper_scissors/rock_paper_scissors.java
+++ Uncategorized DMOJ/Java/rock_paper_scissors/rock_paper_scissors.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 
-public class RockPaperScissors{
+public class rock_paper_scissors{
     public static void main (String[] args){
 
         Scanner sc = new Scanner(System.in);

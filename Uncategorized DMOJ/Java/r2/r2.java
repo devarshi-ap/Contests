735aef76ab85e7e6098932ed79d240ccdcec3f24
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-09-22 13:55:58 -0400

    Create r2.java

---
+++ Uncategorized DMOJ/Java/r2/r2.java
@@ -0,0 +1,19 @@
+//COCI '06 Contest 2 #1 R2
+
+import java.util.Scanner;
+
+
+public class r2 {
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+
+        System.out.println(2*s - r1);
+
+        sc.close();
+    }
+}  
+

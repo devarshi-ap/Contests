f15ef2eae256a493063c3fcbb888103020a0fde5
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-09-24 21:27:02 -0400

    Create ccc08j1_body_mass_index.java

---
+++ CCC Java/ccc08j1_body_mass_index/ccc08j1_body_mass_index.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ccc08j1_body_mass_index
+{
+ public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  
+  
+  double weight = sc.nextDouble();
+  double height = sc.nextDouble();
+  
+  System.out.printf("%n");
+  
+  double bmi = weight/(height*height);
+  
+  if(bmi > 25)
+  {
+    System.out.println("Overweight");
+  }
+  else if(bmi < 18.5)
+  {
+    System.out.println("Underweight");
+  }
+  else
+  {
+    System.out.println("Normal weight");
+  }
+
+ }
+}

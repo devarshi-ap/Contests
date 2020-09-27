1ff221fbbac0ab382b9724245bba84953e430df9
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-09-26 21:33:41 -0400

    Create ccc07j2_I_speak_txtmsg.java

---
+++ CCC Java/ccc07j2_I_speak_txtmsg/ccc07j2_I_speak_txtmsg.java
@@ -0,0 +1,49 @@
+//CCC '07 J2 - I Speak TXTMSG
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+
+public class ccc07j2_I_speak_txtmsg {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        HashMap<String, String> text = new HashMap<String, String>();
+
+        text.put("CU", "see you");
+        text.put(":-)", "I'm happy");
+        text.put(":-(", "I'm unhappy");
+        text.put(";-)", "wink");
+        text.put(":-P", "stick out my tongue");
+        text.put("(~.~)", "sleepy");
+        text.put("TA", "totally awesome");
+        text.put("CCC", "Canadian Computing Competition");
+        text.put("CUZ", "because");
+        text.put("TY", "thank-you");
+        text.put("YW", "you're welcome");
+        text.put("TTYL", "talk to you later");
+
+        
+
+        while(true){
+
+            String msg = sc.nextLine();
+
+            if(msg.equals("TTYL")){
+                System.out.println(text.get(msg));
+                break;
+
+            } else {
+                
+                if(text.containsKey(msg)){
+                    System.out.println(text.get(msg));
+                } else {
+                    System.out.println(msg);
+                }
+            }
+            
+        }
+
+        sc.close();
+    }
+}

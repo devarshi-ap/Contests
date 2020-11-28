41a4e900e8291abb73b64b23457fc1afd02403c6
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-11-28 14:47:58 -0500

    Update returningHome.py

--- CCC Python/2005/Junior 3/returningHome.py
+++ CCC Python/2005/Junior 3/returningHome.py
@@ -1,1 +1,21 @@
+#CCC '05 J3 - Returning Home
 
+d = []
+t = 0
+
+while True:
+    d.append(input())
+    if d[t] == "SCHOOL":
+        break
+    t += 1
+
+for a in range(len(d) - 2, -1, -2):
+    print("Turn ", end = "")
+    if d[a] == "R":
+        print("LEFT ", end = "")
+    else:
+        print("RIGHT ", end = "")
+    if a == 0:
+        print("into your HOME.")
+    else:
+        print("onto " + d[a - 1] + " street.")

aa6829cd0001785b188b2dd2f473e7d5551e9aab
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-12-01 12:00:52 -0500

    Delete ShiftySum.py

--- CCC Python/ccc17j2_shifty_sum/ShiftySum.py
+++
@@ -1,9 +0,0 @@
-n = int(input())    #asks user for number to shift
-k = int(input())    #asks user for number of times to shift N
-sum = n             #holds sum of shifted numbers
-
-for i in range(k):
-    n *= 10
-    sum += n
-    
-print (sum)

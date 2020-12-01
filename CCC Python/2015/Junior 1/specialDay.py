caecefec43d509a729d0c4483a0a8836122356a9
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-12-01 11:54:51 -0500

    Update specialDay.py

--- CCC Python/2015/Junior 1/specialDay.py
+++ CCC Python/2015/Junior 1/specialDay.py
@@ -1,1 +1,17 @@
+month = int(input())
+day = int(input())
 
+if(month < 2):      #before the month of Feb
+    print ("Before")
+
+if(month > 2):    #after the month of Feb
+    print ("After")
+    
+if(month == 2 and day < 18):    #during Feb but before the 18th
+    print ("Before")
+
+if(month == 2 and day > 18):    #during Feb but after the 18th
+    print ("After")
+    
+if(month == 2 and day == 18):   #on Feb 18
+    print ("Special")

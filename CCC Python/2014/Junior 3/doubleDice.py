11dd99d9136d10662297d317319e2526273b7b96
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-12-01 11:54:29 -0500

    Update doubleDice.py

--- CCC Python/2014/Junior 3/doubleDice.py
+++ CCC Python/2014/Junior 3/doubleDice.py
@@ -1,1 +1,16 @@
+#CCC '14 J3 - Double Dice
 
+a = 100
+d = 100
+n = int(input())
+
+for i in range(n):
+    x, y = map(int, input().split())    #takes in x y as distinct elements
+    
+    if x > y:
+        d -= x
+    elif y > x:
+        a -= y
+
+print (a)
+print (d)

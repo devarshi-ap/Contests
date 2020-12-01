4566fc92261d5cdd3c9deaaccd3945e3c54e4fa6
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-12-01 11:50:22 -0500

    Update sumacSequence.py

--- CCC Python/2011/Junior 3/sumacSequence.py
+++ CCC Python/2011/Junior 3/sumacSequence.py
@@ -1,1 +1,13 @@
+#ccc11j3 Sumac Sequences
 
+a = int(input())
+b = int(input())
+count = 2
+
+while a >= b and a >= 0 and b >= 0:
+    count += 1
+    c = a - b
+    a = b
+    b = c
+    
+print (count)

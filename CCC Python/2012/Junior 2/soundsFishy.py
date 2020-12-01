bd86c1a99c0d65267a00b0a750cde2c2a454e137
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-12-01 11:52:19 -0500

    Update soundsFishy.py

--- CCC Python/2012/Junior 2/soundsFishy.py
+++ CCC Python/2012/Junior 2/soundsFishy.py
@@ -1,1 +1,18 @@
+#asks user for 4 depth readings and outputs an approriate message
+#involving fishing
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
 
+if (a > b) and (b > c) and (c > d):
+  print("Fish Diving")
+  
+elif (a < b) and (b < c) and (c < d):
+  print ("Fish Rising")
+  
+elif (a == b) and (b == c) and (c == d):
+  print("Fish At Constant Depth")
+  
+else:
+  print("No Fish")

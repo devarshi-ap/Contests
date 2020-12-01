85f537bc414c8c636a92a123a3e171f88a7f7d20
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-12-01 11:52:49 -0500

    Update rotatingLetters.py

--- CCC Python/2013/Junior 2/rotatingLetters.py
+++ CCC Python/2013/Junior 2/rotatingLetters.py
@@ -1,1 +1,15 @@
+word = input()  #prompt user for word
 
+canRotate = True
+
+#iterates thru each character in the word and identifies the list of characters that can rotate
+for c in word:
+    if(c == 'I' or c == 'O' or c == 'S' or c == 'H' or c == 'Z' or c == 'X' or c == 'N'):
+        canRotate = True
+    else:
+        canRotate = False
+    
+if canRotate:
+    print ("YES")
+else:
+    print ("NO")

5ab4b41bf60beeafb22454fc5060c3a85242073a
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-12-01 11:54:06 -0500

    Update voteCount.py

--- CCC Python/2014/Junior 2/voteCount.py
+++ CCC Python/2014/Junior 2/voteCount.py
@@ -1,1 +1,17 @@
+numVotes = int(input())
+votes = input()
+aCount = 0
+bCount = 0
 
+for i in range(numVotes):
+    if votes[i] == "A":
+        aCount += 1
+    else:
+        bCount += 1
+
+if(aCount > bCount):
+    print ("A")
+elif(bCount > aCount):
+    print ("B")
+else:
+    print ("Tie")

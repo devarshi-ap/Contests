5f69f41250856edfc2008430423e5609c1cf4b93
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-12-02 14:31:07 -0500

    Update 1987to2013.py

--- CCC Python/2013/Senior 1/1987to2013.py
+++ CCC Python/2013/Senior 1/1987to2013.py
@@ -1,1 +1,20 @@
+# CCC '13 S1 - From 1987 to 2013
 
+def isDistinct(year):
+	StringYear = str(year)
+
+	for digit in StringYear:
+		if StringYear.count(digit) > 1:
+			return False
+	
+	return True
+
+
+y = int(input()) + 1	# + 1 to handle start years that are distinct
+
+while True:
+	if isDistinct(y):
+		print(y)
+		break
+	else:
+		y+=1
